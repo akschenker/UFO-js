42f5de8d65fac3790c7bca626f33b67e5cc0d5ae
Arley Schenker <akschenker@gmail.com>
2018-10-29 18:30:32 -0700

    added display for additional filters

--- static/js/app.js
+++ static/js/app.js
@@ -30,3 +30,20 @@ button.on("click", function() {
 
     filtered.forEach(appendRowsAndData);
 });
+
+var moreFilters = d3.select("#more-filter-btn");
+
+moreFilters.on("click", function() {
+    d3.event.preventDefault();
+    
+    // Use for loop to create additional filters
+    var filters = d3.select("#filters");
+    const filterList = ["City", "State", "Country", "Shape","Duration"];
+
+    filterList.forEach(filter => {
+        var newLi = filters.append("li").attr("class","filter list-group-item");
+        newLi.append("label").attr("for", filter).text(`Enter a ${filter}`);
+        newLi.append("input").attr("class", "form-control").attr("type", "text");
+    });
+    moreFilters.style("display", "none");
+});
